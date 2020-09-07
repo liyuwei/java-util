89bc24ad0aaafb057f7ac477c9cc2264be535e28
li.yuwei <li.yuwei@immomo.com>
2020-09-07 17:45:46 +0800

    first commit

---
+++ src/test/java/com/base/javautil/JavaUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.javautil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
